ee9d8d3ad7844628db518f7be3b914cf32ece04f
tyronechen <tyrone.chen@monash.edu>
2022-06-10 16:26:29 +1000

    Add example shell commands

---
+++ src/main.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# main controller script for running the pipeline
+python generate_synthetic.py \
+  ../data/Yeast_promoter.gz \
+  -o ../data/Yeast_promoter_synthetic.fa
+
+python tokenise.py \
+  -i ../data/Yeast_promoter.gz \
+  -t ../results/tmp/yeast.json
+
+python create_dataset.py \
+  ../data/Yeast_promoter.gz \
+  ../data/Yeast_promoter_synthetic.fa \
+  ../results/tmp/yeast.json \
+  -o ../results/tmp/
