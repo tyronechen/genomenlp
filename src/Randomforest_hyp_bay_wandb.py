ac90ade9679498fd3c4b672ba3b6b7753c6c45f9
Navya Tyagi <tyagin.india@gmail.com>
2022-09-28 14:18:19 +0000

    added function for RF classifier implementation

--- src/Randomforest_hyp_bay_wandb.py
+++ src/Randomforest_hyp_bay_wandb.py
@@ -16,109 +16,159 @@ import matplotlib.pyplot as plt
 #wandb
 #!pip install wandb
 import wandb
+#wandb.init('RF classifier')
 
 # Bayesian optimization
 from hyperopt import hp,fmin,tpe,STATUS_OK,Trials
 
+#functions
+def split_dataset(X, Y, train_ratio, test_ratio, validation_ratio):
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=1 - train_ratio)
+    x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio))
+    return x_train, y_train, x_test, y_test, x_val, y_val
+
+def train_model(model, param, x_train, y_train):
+   clf=model(param)
+   # fit the training data into the model
+   clf.fit(x_train, y_train)
+   y_pred=clf.predict(x_test)
+   y_probas=clf.predict_proba(x_test)
+   return clf, y_pred, y_probas
+
+def model_metrics(model, x_test, y_test, y_pred):
+  # accuracy prediction
+  accuracy = accuracy_score(y_test, y_pred)
+  print("Accuracy: %.2f%%" % (accuracy * 100.0))
+  # classification report
+  print("Classification report:\n")
+  print(classification_report(y_test, y_pred))
+  # confusion matrix
+  conf=confusion_matrix(y_test, y_pred)
+  print("Confusion matrix:\n", conf)
+  # confusion matrix plot
+  print("Confusion matrix plot:\n")
+  plot_confusion_matrix(model, x_test, y_test) 
+  plt.show() 
+
+def shap_plot(model, x_test):
+   explainer = shap.TreeExplainer(model)
+   shap_values = explainer.shap_values(x_test)
+   print('SHAP plot for Feature importance')
+   shap.summary_plot(shap_values, x_test, plot_type="bar")
+
+def grid_search(model, param):
+    clf_grid=GridSearchCV(estimator=model, param_grid=param, cv = 3, verbose=2, n_jobs = 4)
+    # fit the training data
+    clf_grid.fit(x_train,y_train)
+    # Best hyperparameter values
+    print('Best parameter values:')
+    print(clf_grid.best_params_)
+    # predicted values from the grid search model
+    clf_g=clf_grid.best_estimator_
+    y_pred=clf_g.predict(x_test)
+    y_probas = clf_g.predict_proba(x_test)
+    return clf_g, y_pred, y_probas
+
+def random_search(model, param):
+    clf_ran=RandomizedSearchCV(estimator=model, param_distributions=param, cv = 3, verbose=2, n_jobs = 4)
+    # fit the training data
+    clf_ran.fit(x_train,y_train)
+    # Best hyperparameter values
+    print('Best parameter values:')
+    print(clf_ran.best_params_)
+    # predicted values from the random search model
+    clf_r=clf_ran.best_estimator_
+    y_pred=clf_r.predict(x_test)
+    y_probas = clf_r.predict_proba(x_test)
+    return clf_r, y_pred, y_probas
+
+def feature_imp(model,feature_names, n_top_features):
+  feats=np.array(feature_names)
+  importances = model.feature_importances_
+  indices = np.argsort(importances)[::-1]
+  for f in range(50):
+    print("%d. feature %d - %s : %f" % (f+1, indices[f], feats[f], importances[indices[f]]))
+  plt.figure(figsize=(8,10))
+  plt.barh(feats[indices][:n_top_features ], importances[indices][:n_top_features ])
+  plt.xlabel("RF feature Importance ")
+
+def token_freq_plot(token, top_features):
+  freq=token.value_counts().nlargest(top_features)
+  freq.plot(kind='bar', title='Token frequency distribution plot')
+
 # data
 # loaded the dna2vec word embedding file data
-# opened and read the respective files
-f_pos=open('Positive.w2v','r')
-f_neg=open('Negative.w2v','r')
-file_p=f_pos.read()
-file_n=f_neg.read()
-# took file content as a list of sequences
-# seperated by newline according to the indexing
-lis_p=[x.split() for x in file_p.split('\n')[1:-1]]
-lis_n=[x.split() for x in file_n.split('\n')[1:-1]]
-# converted the sequence values(string) into numerical values(float) 
-list_p=[[float(x) for x in y[1:]] for y in lis_p]
-list_n=[[float(x) for x in y[1:]] for y in lis_n]
-# labelled natural sequence embeddings as 1
-l_pos=[x+[1] for x in list_p]
-# labelled synthetic sequence embeddings as 0
-l_neg=[x+[0] for x in list_n]
-# merged both the lists together
-l_whole = l_pos+l_neg
-# converted the list to arrray for model implementation
-dataset = np.array([np.array(x) for x in l_whole])
+# importing the embeddings
+pos = pd.read_csv("Positive.w2v", skiprows=1, sep=" ", index_col=0, header=None)
+neg = pd.read_csv("Negative.w2v", skiprows=1, sep=" ", index_col=0, header=None)
+# data preprocessing
+# adding labels for natural and synthetic embeddings
+pos['label']=1
+neg['label']=0
+# dataset
+df=pd.concat([pos,neg])
+#df.index.name='kmer'
+#df.head()
+df[:] = np.nan_to_num(df)
+#Token freq plot
+token_freq_plot(df.index, 30)
 
 # split data into X and Y
 # sequence embeddings
-X = dataset[:,:-1]
+X=np.array(df.drop(['label'], axis=1))
 # label of sequence embeddings
-Y = dataset[:,-1]
+Y=np.array(df['label'])
+# X and Y should have same length
+print(X.shape)
+print(Y.shape)
 
-# split the data into train and test using sklearn
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
-# test_size defines test data to be split from train data
+# split the dataset into train, test and validation set using sklearn
+train_ratio = 0.70
+validation_ratio = 0.15
+test_ratio = 0.15
+# train is now 70% of the entire data set
+# test is now 15% of the initial data set
+# validation is now 15% of the initial data set
+x_train, y_train, x_test, y_test, x_val, y_val=split_dataset(X, Y, train_ratio, test_ratio, validation_ratio) 
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+print(x_val.shape)
+print(y_val.shape)
 
-# RF classifier model
-classifier=RandomForestClassifier()
-
-# fit the training data into the model
-classifier.fit(x_train, y_train)
-
-# predicted values from the model
-y_pred=classifier.predict(x_test)
-y_probas =classifier.predict_proba(x_test)
-
-# accuracy prediction
-accuracy = accuracy_score(y_test, y_pred)
-print("BASE MODEL")
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-# classification report
-print("Classification report:\n")
-print(classification_report(y_test, y_pred))
-
-# confusion matrix
-conf=confusion_matrix(y_test, y_pred)
-print("Confusion matrix:\n", conf)
-
-# confusion matrix plot
-print("Confusion matrix plot:\n")
-plot_confusion_matrix(classifier, x_test, y_test) 
-plt.show() 
-
-# Wandb plots
-# Weights and biases
-import wandb
+print('RF BASE MODEL')
+# training the model
+model=RandomForestClassifier
+param=1000
+rf_base, y_pred, y_probas=train_model(model, param, x_train, y_train)
+# model metrics
+model_metrics(rf_base, x_test, y_test, y_pred)
+# SHAP Plot
+shap_plot(rf_base, x_test) 
+# wandb plot
 wandb.init(project="RF classifier", name="RF-base model")
-# Feature importance
-wandb.sklearn.plot_feature_importances(classifier)
-# metrics summary
-wandb.sklearn.plot_summary_metrics(classifier, x_train, y_train, x_test, y_test)
-# precision recall
-wandb.sklearn.plot_precision_recall(y_test, y_probas, labels=None)
-# ROC curve
-wandb.sklearn.plot_roc(y_test, y_probas, labels=None)
-# Learning curve
-wandb.sklearn.plot_learning_curve(classifier, x_train, y_train)
-# class proportions
-wandb.sklearn.plot_class_proportions(y_train, y_test, labels=None)
-# calibration curve
-wandb.sklearn.plot_calibration_curve(classifier, X, Y, 'RandomForestClassifier- Base model')
-#confusion matrix
-wandb.sklearn.plot_confusion_matrix(y_test, y_pred, labels=None)
-
+# Visualize all classifier plots at once
+wandb.sklearn.plot_classifier(rf_base, x_train, x_test, y_train, y_test, y_pred, y_probas, labels=None, model_name='BASE MODEL', feature_names=None)
+#Feature imp plot
+feature_imp(rf_base, df.index, 50)
 
 # parameters cuurently used
 print('Parameters currently in use:')
-pprint(classifier.get_params())
+pprint(rf_base.get_params())
 
 # Hyperparameter tuning using GridsearchCV
 # Setting range of parameters
 # Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 10, stop = 100, num = 10)]
+n_estimators = [1000, 2000, 3000, 4000, 5000]
 # Number of features to consider at every split
 max_features = ['auto', 'sqrt']
 # Maximum number of levels in tree
-max_depth = [2,4]
+max_depth = [1, 2, 3, 4, 5]
 # Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
+min_samples_split = [1, 2, 3]
 # Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2]
+min_samples_leaf = [1, 2, 3]
 # Method of selecting samples for training each tree
 bootstrap = [True, False]
 # Create the random grid
@@ -131,219 +181,37 @@ param = {'n_estimators': n_estimators,
 print("\nGRID SEARCH MODEL")
 print('Range of parameters used for hyperparameter tuning:')
 pprint(param)
-# implemented grid search on RF classifier
-classifier_grid=GridSearchCV(estimator=classifier, param_grid=param, cv = 3, verbose=2, n_jobs = 4)
-# fit the training data
-classifier_grid.fit(x_train,y_train)
 
-# Best hyperparameter values
-print('Best parameter values:')
-print(classifier_grid.best_params_)
-
-# predicted values from the grid search model
-cl_g=classifier_grid.best_estimator_
-pred=cl_g.predict(x_test)
-y_probas = cl_g.predict_proba(x_test)
-
-
-# accuracy prediction for grid search model
-accuracy = accuracy_score(y_test, pred)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-#classification report
-print("Classification report:\n")
-print(classification_report(y_test, y_pred))
-
-# confusion matrix
-conf_g=confusion_matrix(y_test, pred)
-print("Confusion matrix\n", conf_g)
-
-# confusion matrix plot
-print("Confusion matrix plot:\n")
-plot_confusion_matrix(classifier_grid, x_test, y_test)  
-plot.show()
-
-# Weights and biases plot for Grid Search Model
+# Grid Search model
+rf_grid, y_pred1, y_probas1=grid_search(model, param)
+# model metrics
+model_metrics(rf_grid, x_test, y_test, y_pred1)
+# SHAP Plot
+shap_plot(rf_grid, x_test) 
+# wandb plot
 wandb.init(project="RF classifier", name="RF-gridsearch model")
-# Feature importance
-wandb.sklearn.plot_feature_importances(cl_g)
-# metrics summary
-wandb.sklearn.plot_summary_metrics(cl_g, x_train, y_train, x_test, y_test)
-# precision recall
-wandb.sklearn.plot_precision_recall(y_test, y_probas, labels=None)
-# ROC curve
-wandb.sklearn.plot_roc(y_test, y_probas, labels=None)
-# Learning curve
-wandb.sklearn.plot_learning_curve(cl_g, x_train, y_train)
-# class proportions
-wandb.sklearn.plot_class_proportions(y_train, y_test, labels=None)
-# calibration curve
-wandb.sklearn.plot_calibration_curve(cl_g, X, Y, 'RandomForestClassifier-Grid search model')
-# confusion matrix
-wandb.sklearn.plot_confusion_matrix(y_test, y_pred, labels=None)
+# Visualize all classifier plots at once
+wandb.sklearn.plot_classifier(rf_grid, x_train, x_test, y_train, y_test, y_pred1, y_probas1, labels=None, model_name='Grid Search Model', feature_names=None)
+#Feature imp plot
+feature_imp(rf_grid, df.index, 50)
 
-
-# Random search implementation
-# Module for hyperparameter tuning
-# Hyperparameter tuning using RandomizedsearchCV
-# Setting range of parameters
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-# Create the random grid
-param = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap}
 print("\nRANDOM SEARCH MODEL")
 print('Range of parameters used for hyperparameter tuning:')
 pprint(param)
-
-# implemented grid search on RF classifier
-classifier_random=RandomizedSearchCV(estimator = classifier, param_distributions = param, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-
-# fit the training data in the randomized model
-classifier_random.fit(x_train, y_train)
-
-# Best hyperparameter values
-print('Best parameter values:')
-print(classifier_random.best_params_)
-
-# predicted values from the random search model using best parameters
-cl_r=classifier_random.best_estimator_
-pred=cl_r.predict(x_test)
-y_probas = cl_r.predict_proba(x_test)
-
-# accuracy prediction for random search model
-accuracy = accuracy_score(y_test, pred)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-#classification report
-print("Classification report:\n")
-print(classification_report(y_test, y_pred))
-
-# confusion matrix
-conf_r=confusion_matrix(y_test, pred)
-print("Confusion matrix\n", conf_r)
-
-# confusion matrix plot
-print("Confusion matrix plot:\n")
-plot_confusion_matrix(classifier_random, x_test, y_test)  
-plt.show()
-
-# Wandb plot for Randomized Search Model
+# Random search model
+rf_ran, y_pred2, y_probas2=random_search(rf_base, param)
+# model metrics
+model_metrics(rf_ran, x_test, y_test, y_pred2)
+# SHAP Plot
+shap_plot(rf_ran, x_test) 
+# wandb plot
 wandb.init(project="RF classifier", name="RF-random search model")
-# Feature importance
-wandb.sklearn.plot_feature_importances(cl_r)
-# metrics summary
-wandb.sklearn.plot_summary_metrics(cl_r, x_train, y_train, x_test, y_test)
-# precision recall
-wandb.sklearn.plot_precision_recall(y_test, y_probas, labels=None)
-# ROC curve
-wandb.sklearn.plot_roc(y_test, y_probas, labels=None)
-# Learning curve
-wandb.sklearn.plot_learning_curve(cl_r, x_train, y_train)
-# class proportions
-wandb.sklearn.plot_class_proportions(y_train, y_test, labels=None)
-# calibration curve
-wandb.sklearn.plot_calibration_curve(cl_r, X, Y, 'RandomForestClassifier-Random search model')
-# confusion matrix
-wandb.sklearn.plot_confusion_matrix(y_test, y_pred, labels=None)
+# Visualize all classifier plots at once
+wandb.sklearn.plot_classifier(rf_ran, x_train, x_test, y_train, y_test, y_pred2, y_probas2, labels=None, model_name='Random Search Model', feature_names=None)
+#Feature imp plot
+feature_imp(rf_ran, df.index, 50)
 
-# Bayesian optimization
-print("\nBAYESIAN OPTIMIZATION")
-space = {'criterion': hp.choice('criterion', ['entropy', 'gini']),
-        'max_depth': hp.quniform('max_depth', 10, 1200, 10),
-        'max_features': hp.choice('max_features', ['auto', 'sqrt','log2', None]),
-        'min_samples_leaf': hp.uniform('min_samples_leaf', 0, 0.5),
-        'min_samples_split' : hp.uniform ('min_samples_split', 0, 1),
-        'n_estimators' : hp.choice('n_estimators', [10, 50, 300, 750, 1200,1300,1500])
-    }
-def objective(space):
-   model = RandomForestClassifier(criterion = space['criterion'], max_depth = space['max_depth'],
-                                 max_features = space['max_features'],
-                                 min_samples_leaf = space['min_samples_leaf'],
-                                 min_samples_split = space['min_samples_split'],
-                                 n_estimators = space['n_estimators'], 
-                                 )
-   #5 times cross validation fives 5 accuracies=>mean of these accuracies will be considered
-   accuracy = cross_val_score(model, x_train, y_train, cv = 5).mean()
-   # We aim to maximize accuracy, therefore we return it as a negative value
-   return {'loss': -accuracy, 'status': STATUS_OK }
-
-from sklearn.model_selection import cross_val_score
-trials = Trials()
-best = fmin(fn= objective,
-            space= space,
-            algo= tpe.suggest,
-            max_evals = 80,
-            trials= trials)   
-crit = {0: 'entropy', 1: 'gini'}
-feat = {0: 'auto', 1: 'sqrt', 2: 'log2', 3: None}
-est = {0: 10, 1: 50, 2: 300, 3: 750, 4: 1200,5:1300,6:1500}
-print("Best parameters:")
-print(crit[best['criterion']])
-print(feat[best['max_features']])
-print(est[best['n_estimators']])
-rf_bayesian=RandomForestClassifier(criterion = crit[best['criterion']], max_depth = best['max_depth'], 
-                                       max_features = feat[best['max_features']], 
-                                       min_samples_leaf = best['min_samples_leaf'], 
-                                       min_samples_split = best['min_samples_split'], 
-                                       n_estimators = est[best['n_estimators']]).fit(x_train,y_train)
-
-# predicted values from the model
-pred_b=rf_bayesian.predict(x_test)
-y_probas = rf_bayesian.predict_proba(x_test)
-
-# accuracy prediction
-accuracy = accuracy_score(y_test, pred_b)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
-
-# classification report
-print("Classification report:\n")
-print(classification_report(y_test, pred_b))
-
-# confusion matrix
-conf=confusion_matrix(y_test, pred_b)
-print("Confusion matrix:\n", conf)
-
-# confusion matrix plot
-print("Confusion matrix plot:\n")
-plot_confusion_matrix(rf_bayesian, x_test, y_test)  
-plt.show()
-
-# Wandb plot for Bayesian Optimization
-wandb.init(project="RF classifier", name="RF-Bayesian optimization model")
-# Feature importance
-wandb.sklearn.plot_feature_importances(model=rf_bayesian, title="RF-Bayesian model")
-# metrics summary
-wandb.sklearn.plot_summary_metrics(rf_bayesian, x_train, y_train, x_test, y_test)
-# precision recall
-wandb.sklearn.plot_precision_recall(y_test, y_probas, labels=None)
-# ROC curve
-wandb.sklearn.plot_roc(y_test, y_probas, labels=None)
-# Learning curve
-wandb.sklearn.plot_learning_curve(rf_bayesian, x_train, y_train)
-# class proportions
-wandb.sklearn.plot_class_proportions(y_train, y_test, labels=None)
-# calibration curve
-wandb.sklearn.plot_calibration_curve(rf_bayesian, X, Y, 'RandomForestClassifier-Bayesian optimization model')
-# confusion matrix
-wandb.sklearn.plot_confusion_matrix(y_test, y_pred, labels=None)
-
-
+# Bayesian
 # Exporting metrics from a project in to a CSV file
 
 api = wandb.Api()
