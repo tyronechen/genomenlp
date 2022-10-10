4007b535b794ac813fce2d2f9b7018d8e7b06872
tyronechen <tyrone.chen@monash.edu>
2022-10-10 17:50:31 +1100

    Cross validation now groups runs correctly

--- src/cross_validate.py
+++ src/cross_validate.py
@@ -203,6 +203,7 @@ def main():
         })
         wandb.init(
             group=args.group_name,
+            job_type=args.group_name,
             settings=wandb.Settings(console='off', start_method='fork'),
             entity=args.entity_name
             )
@@ -221,21 +222,17 @@ def main():
         print("Saving model to:", wandb.run.dir)
         trainer.save_model(wandb.run.dir)
         wandb.save(os.path.join(wandb.run.dir, "pytorch_model.bin"))
-
     wandb.finish()
 
-    # TODO: get metrics from group
-    sys.exit()
-
-    # connect to the finished sweep using API
     api = wandb.Api()
-    entity_project_id = "/".join([entity_name, project_name, sweep_id])
-    sweep = api.sweep(entity_project_id)
-    print("Entity / Project / Sweep ID:", sweep)
+    entity_project_id = "/".join([entity_name, project_name])
+    runs = api.runs(path="/".join([entity_name, project_name]),
+                    filters={"group_name": args.group_name})
+
+    print("Entity / Project / Group ID:", entity_project_id, args.group_name)
 
     # download metrics from all runs
     print("Get metrics from all runs")
-    runs = api.runs("/".join([entity_name, project_name]))
     summary_list, config_list, name_list = [], [], []
     for run in runs:
         # .summary contains the output keys/values for metrics
@@ -255,212 +252,28 @@ def main():
         })
     runs_df.to_csv("/".join([args.output_dir, "metrics.csv"]))
 
-    # hyperparameter tuning on 10% of original datasett following:
-    # https://github.com/huggingface/notebooks/blob/main/examples/text_classification.ipynb
+    # TODO: to load training args as a json file of args later
+    # if os.path.exists(hyperparameter_sweep):
+    #     print(" ".join(["Loading sweep hyperparameters from",
+    #                    hyperparameter_sweep]))
+    #     with open(hyperparameter_sweep, 'r') as infile:
+    #         sweep_config = json.load(infile)
 
-    # if hyperparameter sweep is provided or set to True,
-    if wandb_state == True:
-        if os.path.exists(hyperparameter_sweep):
-            print(" ".join(["Loading sweep hyperparameters from",
-                           hyperparameter_sweep]))
-            with open(hyperparameter_sweep, 'r') as infile:
-                sweep_config = json.load(infile)
-        else:
-            warn("No sweep hyperparameters provided, do random search")
-            # method
-            sweep_config = {
-                'name': 'random',
-                'method': 'random',
-                "metric": {
-                    "name": "eval/f1",
-                    "goal": "maximize"
-                    },
-                'parameters': {
-                    'epochs': {
-                        'values': [1, 2, 3, 4, 5]
-                        },
-                    'dropout': {
-                      "values": [0.15, 0.2, 0.25, 0.3, 0.4]
-                    },
-                    'batch_size': {
-                        'values': [8, 16, 32, 64]
-                        },
-                    'learning_rate': {
-                        'distribution': 'log_uniform_values',
-                        'min': 1e-5,
-                        'max': 1e-1
-                    },
-                    'weight_decay': {
-                        'values': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5]
-                    },
-                    'decay': {
-                        'values': [1e-5, 1e-6, 1e-7]
-                    },
-                    'momentum': {
-                        'values': [0.8, 0.9, 0.95]
-                    }
-                },
-                "early_terminate": {
-                    "type": "hyperband",
-                    "s": 2,
-                    "eta": 3,
-                    "max_iter": 27
-                }
-            }
-
-        sweep_id = wandb.sweep(sweep_config,
-                               entity=entity_name,
-                               project=project_name)
-
-        # function needs to be defined internally to access namespace
-        def _sweep_wandb(config=None):
-            with wandb.init(
-                config=config,
-                settings=wandb.Settings(console='off', start_method='fork'),
-                entity=args.entity_name
-                ):
-                # set sweep configuration
-                config = wandb.config
-                sweep_dir = "/".join([args.output_dir, sweep_config["name"]])
-                print("\nLOGGING OUTPUT TO:\n", sweep_dir, "\n")
-                # set training arguments
-                training_args = TrainingArguments(
-                    output_dir=sweep_dir,
-            	    report_to='wandb',  # Turn on Weights & Biases logging
-                    num_train_epochs=config.epochs,
-                    learning_rate=config.learning_rate,
-                    weight_decay=config.weight_decay,
-                    per_device_train_batch_size=config.batch_size,
-                    per_device_eval_batch_size=config.batch_size,
-                    save_strategy='epoch',
-                    evaluation_strategy='epoch',
-                    logging_strategy='epoch',
-                    load_best_model_at_end=True,
-                    remove_unused_columns=False,
-                    fp16=fp16,
-                    bf16=False,
-                )
-                # define training loop
-                trainer = Trainer(
-                    model_init=_model_init,
-                    args=training_args,
-                    tokenizer=tokeniser,
-                    train_dataset=dataset['train'],
-                    eval_dataset=dataset['valid'],#.shard(index=1, num_shards=10),
-                    compute_metrics=_compute_metrics,
-                    data_collator=data_collator,
-                )
-                # start training loop
-                trainer.train()
-                print("Saving model to:", wandb.run.dir)
-                trainer.save_model(wandb.run.dir)
-                wandb.save(os.path.join(wandb.run.dir, "pytorch_model.bin"))
-
-        # allow passing of argument with variable outside namespace
-        # wandb_train_func = functools.partial(_sweep_wandb, sweep_config)
-        wandb.agent(sweep_id,
-                    # function=wandb_train_func,
-                    function=_sweep_wandb,
-                    count=sweep_count)
-        wandb.finish()
-
-        # connect to the finished sweep using API
-        api = wandb.Api()
-        entity_project_id = "/".join([entity_name, project_name, sweep_id])
-        sweep = api.sweep(entity_project_id)
-        print("Entity / Project / Sweep ID:", sweep)
-
-        # download metrics from all runs
-        print("Get metrics from all runs")
-        runs = api.runs("/".join([entity_name, project_name]))
-        summary_list, config_list, name_list = [], [], []
-        for run in runs:
-            # .summary contains the output keys/values for metrics
-            #  We call ._json_dict to omit large files
-            summary_list.append(run.summary._json_dict)
-            # .config contains the hyperparameters.
-            #  We remove special values that start with _.
-            config_list.append(
-                {k: v for k,v in run.config.items()
-                 if not k.startswith('_')})
-            # .name is the human-readable name of the run
-            name_list.append(run.name)
-        runs_df = pd.DataFrame({
-            "summary": summary_list,
-            "config": config_list,
-            "name": name_list
-            })
-        runs_df.to_csv("/".join([args.output_dir, "metrics.csv"]))
-
-        # download best model file from the sweep
-        # these two lines dont work for some reason
-        # best_run = sweep.best_run()
-        # print(best_run)
-        metric_opt = sweep_config["metric"]["name"]
-        runs = sorted(
-            sweep.runs,
-            key=lambda run: run.summary.get(metric_opt, 0),
-            reverse=True
-            )
-        print("Get best model file from the sweep:", runs[0])
-        score = runs[0].summary.get(metric_opt, 0)
-        print(f"Best run {runs[0].name} with {metric_opt}={score}%")
-        best_model = "/".join([args.output_dir, "model_files"])
-        for i in runs[0].files():
-            i.download(root=best_model, replace=True)
-        print("\nBEST MODEL AND CONFIG FILES SAVED TO:\n", best_model)
-        print("\nHYPERPARAMETER SWEEP END")
-    else:
-        warn("wandb hyperparameter tuning is disabled, using 🤗 tuner.")
-        trainer = Trainer(
-            model_init=_model_init,
-            tokenizer=tokeniser,
-            args=args_train,
-            train_dataset=dataset["train"],#.shard(index=1, num_shards=100),
-            eval_dataset=dataset["valid"],
-            data_collator=data_collator,
-            # disable_tqdm=args.disable_tqdm,
-            # compute_metrics=_compute_metrics,
+    # download best model file from the sweep
+    # these two lines dont work for some reason
+    metric_opt = sweep_config["metric"]["name"]
+    runs = sorted(
+        runs, key=lambda run: run.summary.get(metric_opt, 0), reverse=True
         )
-        reporter = CLIReporter(
-            parameter_columns={
-                "weight_decay": "w_decay",
-                "learning_rate": "lr",
-                "per_device_train_batch_size": "train_bs/gpu",
-                "num_train_epochs": "num_epochs",
-            },
-            metric_columns=["eval_acc", "eval_loss",
-                            "epoch", "training_iteration"],
-        )
-        best_run = trainer.hyperparameter_search(
-            n_trials=10,
-            direction="maximize",
-            backend="ray",
-            resources_per_trial={"cpu": hyperparameter_cpus, "gpu": 0},
-            # stop={"training_iteration": 1} if smoke_test else None,
-            progress_reporter=reporter,
-            # scheduler=scheduler,
-            # local_dir="".join([args.output_dir, "./ray_results/"]),
-            log_to_file=True,
-            )
-
-        print("\nTUNED:\n", best_run.hyperparameters, "\n")
-        tuned_path = "".join(
-            [args.output_dir, "/tuned_hyperparameters.json"]
-            )
-        with open(tuned_path, 'w', encoding='utf-8') as f:
-            json.dump(
-                best_run.hyperparameters, f, ensure_ascii=False, indent=4
-                )
-        print("\nHYPERPARAMETER SWEEP END")
-        warn_tune = "".join([
-            "It is not possible to pass tuned hyperparameters \
-            directly due to a bug in how the random number generation \
-            seed is handled! The tuned hyperparameters are output to:",
-            "\n", tuned_path,
-        ])
-        warn(warn_tune)
-
+    # NOTE: it doesnt make sense to download the best model in cross validation
+    # print("Get best model file from the sweep:", runs[0])
+    score = runs[0].summary.get(metric_opt, 0)
+    print(f"Best run {runs[0].name} with {metric_opt}={score}%")
+    # best_model = "/".join([args.output_dir, "model_files"])
+    # for i in runs[0].files():
+    #     i.download(root=best_model, replace=True)
+    # print("\nBEST MODEL AND CONFIG FILES SAVED TO:\n", best_model)
+    # print("\nCROSS VALIDATION END")
 
 if __name__ == "__main__":
     main()
