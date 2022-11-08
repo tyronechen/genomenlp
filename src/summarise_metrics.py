ea439050ae9157c6735def413f520a2ad3987c38
tyronechen <tyrone.chen@monash.edu>
2022-11-08 17:14:15 +1100

    Rework logic flow and wandb API connection

--- src/summarise_metrics.py
+++ src/summarise_metrics.py
@@ -8,7 +8,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import wandb
-from utils import export_run_metrics
+from tqdm import tqdm
+from utils import export_run_metrics, calculate_auc
 
 def main():
     parser = argparse.ArgumentParser(
@@ -16,14 +17,16 @@ def main():
         )
     parser.add_argument('-o', '--output_dir', type=str, default="./plots_out",
                         help='specify path for output (DEFAULT: ./plots_out)')
-    parser.add_argument('-i', '--infile_path', type=str, default=None,
-                        help='path to metrics.csv file')
+    parser.add_argument('-i', '--infile_path', type=str, default=None, nargs="+",
+                        help='path to metrics.csv file(s)')
+    parser.add_argument('-a', '--auc_scores', type=str, default=None, nargs="+",
+                        help='path to auc_scores.csv file(s)')
     parser.add_argument('-r', '--wandb_runs', type=str, default=None, nargs="+",
-                        help='wandb run id to evaluate')
+                        help='wandb run id(s) to evaluate')
     parser.add_argument('-e', '--entity_name', type=str, default="",
                         help='provide wandb team name (if available).')
-    parser.add_argument('-g', '--group_name', type=str, default=None,
-                        help='provide wandb group name (if desired).')
+    parser.add_argument('-g', '--group_name', type=str, default=None, nargs="+",
+                        help='provide wandb group name(s) (if desired).')
     parser.add_argument('-p', '--project_name', type=str, default="",
                         help='provide wandb project name (if available).')
     parser.add_argument('-m', '--metrics', type=str, nargs="+", default=[
@@ -35,33 +38,73 @@ def main():
     args = parser.parse_args()
     output_dir = args.output_dir
     infile_path = args.infile_path
+    auc_scores = args.auc_scores
     wandb_runs = args.wandb_runs
     entity_name = args.entity_name
     group_name = args.group_name
     project_name = args.project_name
     metrics = args.metrics
 
-    input_args = [infile_path, wandb_runs, group_name]
+    input_args = [
+        infile_path, wandb_runs, all([entity_name, project_name])
+        ]
     if not any(input_args):
-        raise OSError("Must provide one of infile_path/wandb_runs/group_name!")
-    if input_args.count(None) <= 1:
-        raise OSError("Must provide ONE of infile_path/wandb_runs/group_name!")
-
+        raise OSError("Provide (infile_path) OR (entity_name project_name [group_name] [wandb_runs]))")
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
     # connect to the finished runs using API
-    api = wandb.Api()
+    api = wandb.Api(timeout=10000)
 
     # download metrics from all specified runs
-    print("Get metrics from all associated runs")
+    print("\nGET ALL ASSOCIATED RUNS\n", entity_name, project_name, group_name)
     runs = None
-    if entity_name != "" or project_name != "":
-        if group_name == None:
-            runs = api.runs(path="/".join([entity_name, project_name]),)
-        else:
-            runs = api.runs(path="/".join([entity_name, project_name]),
-                            filters={"group": group_name})
+    if group_name == None:
+        group_name = ["ungrouped"]
+        runs = api.runs(path="/".join([entity_name, project_name]),)
+        print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT\n")
         if wandb_runs != None:
             runs = [api.run(path="/".join([entity_name, project_name, i]))
                     for i in wandb_runs]
+            print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT AND RUN_ID\n")
+    else:
+        runs = [api.runs(path="/".join([entity_name, project_name,]),
+                         filters={"group": i}) for i in group_name]
+        print("\nGET GROUPED RUNS FROM PROJECT AND ENTITY\n")
+
+    for i in runs:
+        for j in i:
+            print(j)
+
+    if auc_scores == None:
+        print("\nCALCULATE AUC\n")
+        auc = [[[calculate_auc(i, j) for i in run] for run in runs]
+                 for j in tqdm(group_name)]
+        auc = [x for y in [i for j in auc for i in j] for x in y]
+        auc = pd.concat(auc).reset_index().drop(["index"], axis=1)
+        auc.to_csv("/".join([output_dir,"auc_scores.tsv"]),index=False,sep="\t")
+    else:
+        auc = pd.read_csv(auc_scores, sep="\t")
+
+    fig = sns.violinplot(
+        data=auc,
+        x="group_name",
+        y="auc",
+        inner="box",
+        cut=0,
+        hue="class",
+        split=True
+        )
+    fig = fig.get_figure().savefig("/".join([output_dir, "auc_violinplot.pdf"]))
+    plt.clf()
+    fig = sns.boxplot(
+        data=auc,
+        x="group_name",
+        y="auc",
+        hue="class",
+        )
+    fig = fig.get_figure().savefig("/".join([output_dir, "auc_boxplot.pdf"]))
+    plt.show()
+    plt.clf()
 
     if runs != None:
         # this code was adapted directly from the wandb export API for python
@@ -71,12 +114,26 @@ def main():
     if infile_path == None:
         infile_path = "/".join([output_dir, "metrics.csv"])
 
-    data = pd.read_csv(infile_path, index_col=0)
-    data["summary"] = data["summary"].apply(eval)
-    for i in metrics:
-        data[i] = data["summary"].apply(lambda x: x[i])
+    if type(infile_path) is list:
+    # if we give a list of existing files
+        data = [pd.read_csv(i, index_col=0) for i in infile_path]
+        # data = [[i["infile_path"] := i[j]] for j in infile_path]
+        data = dict(zip(infile_path, data))
+        for i in infile_path:
+            data[i]["infile_path"] = i
+        data = pd.concat(data.values())
+        data["summary"] = data["summary"].apply(eval)
+        print(data.columns)
+        for i in metrics:
+            data[i] = data["summary"].apply(lambda x: x[i])
+    else:
+    # if we just one to plot one based on a run or group id
+        data = pd.read_csv(infile_path, index_col=0)
+        data["infile_path"] = "infile_path"
+        data["summary"] = data["summary"].apply(eval)
+        for i in metrics:
+            data[i] = data["summary"].apply(lambda x: x[i])
 
-    # TODO: write plotting functions given the above metrics
 
 if __name__ == "__main__":
     main()
