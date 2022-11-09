eedc9290a737272a10eb1463d02adec1c041e7bc
tyronechen <tyrone.chen@monash.edu>
2022-11-09 15:09:08 +1100

    Polish plots of metrics and improve logic flow to skip redundant steps

--- src/summarise_metrics.py
+++ src/summarise_metrics.py
@@ -45,35 +45,42 @@ def main():
     project_name = args.project_name
     metrics = args.metrics
 
+    print("\n\nARGUMENTS:\n", args, "\n\n")
+
     input_args = [
         infile_path, wandb_runs, all([entity_name, project_name])
         ]
     if not any(input_args):
-        raise OSError("Provide (infile_path) OR (auc_scores) OR (entity_name project_name [group_name] [wandb_runs]))")
+        raise IOError("Provide (infile_path) OR (auc_scores) OR (entity_name project_name [group_name] [wandb_runs]))")
+    if any([infile_path, auc_scores]) and any([entity_name, project_name, wandb_runs]):
+        warn("If input file(s) and run ids are provided, input file(s) will override runs!")
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     # connect to the finished runs using API
     api = wandb.Api(timeout=10000)
 
-    # download metrics from all specified runs
-    print("\nGET ALL ASSOCIATED RUNS\n", entity_name, project_name, group_name)
+    # download metrics from all specified runs if not already available
     runs = None
-    if group_name == None:
-        group_name = ["ungrouped"]
-        runs = api.runs(path="/".join([entity_name, project_name]),)
-        print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT\n")
-        if wandb_runs != None:
-            runs = [api.run(path="/".join([entity_name, project_name, i]))
-                    for i in wandb_runs]
-            print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT AND RUN_ID\n")
-    else:
-        runs = [api.runs(path="/".join([entity_name, project_name,]),
-                         filters={"group": i}) for i in group_name]
-        print("\nGET GROUPED RUNS FROM PROJECT AND ENTITY\n")
+    if not all([infile_path, auc_scores]):
+        print("\nGET ALL ASSOCIATED RUNS\n", entity_name, project_name, group_name)
+        if group_name == None:
+            group_name = ["ungrouped"]
+            runs = api.runs(path="/".join([entity_name, project_name]),)
+            print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT\n")
+            if wandb_runs != None:
+                runs = [api.run(path="/".join([entity_name, project_name, i]))
+                        for i in wandb_runs]
+                print("\nGET UNGROUPED RUNS FROM ENTITY AND PROJECT AND RUN_ID\n")
+        else:
+            runs = [api.runs(path="/".join([entity_name, project_name,]),
+                             filters={"group": i}) for i in group_name]
+            print("\nGET GROUPED RUNS FROM PROJECT AND ENTITY\n")
 
-    for i in runs:
-        for j in i:
-            print(j)
+        for i in runs:
+            for j in i:
+                print(j)
+    else:
+        print("\nALL RUN METRICS PROVIDED, SKIP DOWNLOAD\n", infile_path, auc_scores)
 
     if auc_scores == None:
         print("\nCALCULATE AUC\n")
@@ -86,6 +93,9 @@ def main():
         auc = [pd.read_csv(x, sep="\t") for x in auc_scores]
         auc = pd.concat(auc)
 
+    violinplot_path = "/".join([output_dir, "metrics_violinplot.pdf"])
+    boxplot_path = "/".join([output_dir, "metrics_boxplot.pdf"])
+    print("\nPLOT AUC SCORES\n", violinplot_path, boxplot_path)
     fig = sns.violinplot(
         data=auc,
         x="group_name",
@@ -95,6 +105,9 @@ def main():
         hue="class",
         split=True
         )
+    fig.set_xticklabels(fig.get_xticklabels(), rotation=45)
+    plt.ylim(0, 1)
+    plt.tight_layout()
     fig = fig.get_figure().savefig("/".join([output_dir, "auc_violinplot.pdf"]))
     plt.clf()
     fig = sns.boxplot(
@@ -103,6 +116,9 @@ def main():
         y="auc",
         hue="class",
         )
+    fig.set_xticklabels(fig.get_xticklabels(), rotation=45)
+    plt.ylim(0, 1)
+    plt.tight_layout()
     fig = fig.get_figure().savefig("/".join([output_dir, "auc_boxplot.pdf"]))
     plt.clf()
 
@@ -136,6 +152,35 @@ def main():
         data["summary"] = data["summary"].apply(eval)
         for i in metrics:
             data[i] = data["summary"].apply(lambda x: x[i])
+
+    print("\nPLOT EVAL METRICS\n", output_dir, metrics)
+
+    for i in metrics:
+        i_ = i.replace("/", "_")
+        vio_path = "".join([output_dir, "/metrics_violinplot_", i_, ".pdf"])
+        box_path = "".join([output_dir, "/metrics_boxplot_", i_, ".pdf"])
+        fig = sns.violinplot(
+            data=data,
+            x="group_name",
+            y=i,
+            inner="box",
+            cut=0,
+            )
+        fig.set_xticklabels(fig.get_xticklabels(), rotation=45)
+        plt.ylim(0, 1)
+        plt.tight_layout()
+        fig = fig.get_figure().savefig(vio_path)
+        plt.clf()
+        fig = sns.boxplot(
+            data=data,
+            x="group_name",
+            y=i,
+            )
+        fig.set_xticklabels(fig.get_xticklabels(), rotation=45)
+        plt.ylim(0, 1)
+        plt.tight_layout()
+        fig = fig.get_figure().savefig(box_path)
+        plt.clf()
 
 if __name__ == "__main__":
     main()
