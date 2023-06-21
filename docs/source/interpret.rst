db5506310ffe9ffbaaabe1dad56d2f5f0d5548ae
Tyrone Chen <tyrone.chen@monash.edu>
2023-06-21 15:20:31 +1000

    Visualise class attributions from input

---
+++ docs/source/interpret.rst
@@ -0,0 +1,78 @@
+Get class attribution for deep learning models
+==============================================
+
+This explains the use of ``interpret.py`` for deep learning through ``genomicBERT``.
+
+Requirements
+------------
+
+All available via ``pip install``::
+
+  python==3.9.15
+  screed==1.0.5
+  tokenizers==0.11.6
+  tqdm==4.64.0
+  transformers==4.18.0
+  transformers-interpret==0.8.1
+  wandb==0.13.4
+
+Source data
+-----------
+
+Source data is a path to a trained ``pytorch`` classifier model directory OR a ``wandb`` run.
+
+
+Results
+-------
+
+.. NOTE::
+
+  Entry points are available if this is installed using the automated conda method. You can then use the command line argument directly, for example: ``create_dataset_bio``. If not, you will need to use the script directly, which follows the same naming pattern, for example: ``python create_dataset_bio.py``.
+
+Running the code as below:
+
+Deep learning
++++++++++++++
+
+Input sequences can be provided as multiple strings and/or fasta files. If a string is provided, the file name will be the first 16 characters of the string followed by a unique string. If a fasta file is provided, the file name(s) will be the fasta header. Label names must be sorted in the order of labels, eg category 1, category 2.
+
+::
+
+  python interpret.py <MODEL_PATH> <INPUT_SEQS ...> [TOKENISER_PATH] [OUTPUT_DIR] [LABEL_NAMES ...]
+
+
+Notes
+-----
+
+More information on `transformers interpretability is available here`_.
+
+.. _transformers interpretability is available here: https://github.com/cdpierse/transformers-interpret
+
+Usage
+-----
+
+genomicBERT: Deep learning
+++++++++++++++++++++++++++
+
+Sequences to test for class attribution can be provided directly or as fasta files.
+
+::
+
+    python interpret.py -h
+    usage: interpret.py [-h] [-t TOKENISER_PATH] [-o OUTPUT_DIR] [-l LABEL_NAMES [LABEL_NAMES ...]]
+                        model_path input_seqs [input_seqs ...]
+
+    Take complete classifier and calculate feature attributions.
+
+    positional arguments:
+        model_path            path to local model directory OR wandb run
+        input_seqs            input sequence(s) directly and/or fasta files
+
+    optional arguments:
+        -h, --help            show this help message and exit
+        -t TOKENISER_PATH, --tokeniser_path TOKENISER_PATH
+                                path to tokeniser.json file to load data from
+        -o OUTPUT_DIR, --output_dir OUTPUT_DIR
+                                specify path for output (DEFAULT: ./interpret_out)
+        -l LABEL_NAMES [LABEL_NAMES ...], --label_names LABEL_NAMES [LABEL_NAMES ...]
+                                provide label names matching order (DEFAULT: None).
---
+++ src/ziran/interpret.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# make sure utils can be imported
+import inspect
+import os
+import sys
+sys.path.insert(0, os.path.dirname(inspect.getfile(lambda: None)))
+
+# get feature scores which weight a feature towards a class
+import argparse
+import contextlib
+from hashlib import md5
+from time import time
+from warnings import warn
+import screed
+import wandb
+from tqdm import tqdm
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers_interpret import SequenceClassificationExplainer
+
+def main():
+    parser = argparse.ArgumentParser(
+         description='Take complete classifier and calculate feature attributions.'
+        )
+    parser.add_argument('model_path', type=str,
+                        help='path to local model directory OR wandb run')
+    parser.add_argument('input_seqs', type=str, nargs="+",
+                        help='input sequence(s) directly and/or fasta files')    
+    parser.add_argument('-t', '--tokeniser_path', type=str, default="",
+                        help='path to tokeniser.json file to load data from')
+    parser.add_argument('-o', '--output_dir', type=str, default="./interpret_out",
+                        help='specify path for output (DEFAULT: ./interpret_out)')
+    parser.add_argument('-l', '--label_names', type=str, default=None, nargs="+",
+                        help='provide label names matching order (DEFAULT: None).')
+
+    args = parser.parse_args()
+    model_path = args.model_path
+    input_seqs = args.input_seqs
+    tokeniser_path = args.tokeniser_path
+    label_names = args.label_names
+    output_dir = args.output_dir
+
+    print("\n\nARGUMENTS:\n", args, "\n\n")
+
+    if os.path.exists(output_dir):
+        warn(" ".join(["Overwriting files in:", output_dir]))
+    else:
+        os.mkdir(output_dir)
+
+    if os.path.exists(model_path):
+        print("Use model files from:", model_path)
+    else:
+        print("Downloading model files from wandb:", model_path)
+        api = wandb.Api(timeout=10000)
+        run = api.run(path=model_path)
+        for i in tqdm(run.files(), desc="Downloading model files from wandb"):
+            i.download(root=output_dir, replace=True) 
+        model_path = output_dir
+    
+    if os.path.exists(tokeniser_path):
+        print("Using custom tokeniser from:", tokeniser_path)
+    else:
+        print("Use tokeniser from:", model_path)
+        tokeniser_path = model_path
+    
+    model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    tokeniser = AutoTokenizer.from_pretrained(tokeniser_path)
+
+    cls_explainer = SequenceClassificationExplainer(
+        model, tokeniser, custom_labels=label_names
+        )
+
+    for i in tqdm(input_seqs, desc="Calculating class attributions"):
+        if os.path.exists(i):
+            with screed.open(i) as infile:
+                for j in infile:
+                    cls_explainer(j.sequence.upper())
+                    with contextlib.redirect_stdout(None):            
+                        cls_explainer.visualize(
+                            "".join([output_dir, "/", j.name, ".html"])
+                            )
+        else:
+            cls_explainer(i)
+            unique = md5(str(time()).encode("utf-8")).hexdigest()[:16]
+            with contextlib.redirect_stdout(None):
+                cls_explainer.visualize(
+                    "".join([output_dir, "/", i[:16], "_", unique, ".html"])
+                    )
+
+if __name__ == "__main__":
+    main()
